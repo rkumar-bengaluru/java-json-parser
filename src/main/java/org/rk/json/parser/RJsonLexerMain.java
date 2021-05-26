ae50088d2ee1d1f758817ef0a7722b7b5ae3bac7
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-05-26 09:26:48 +0530

    cleanup

--- src/main/java/org/rk/json/parser/RJsonLexerMain.java
+++ src/main/java/org/rk/json/parser/RJsonLexerMain.java
@@ -6,10 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -20,7 +17,7 @@ import org.junit.AfterClass;
 import org.junit.Before;  
 import org.junit.BeforeClass;  
 import org.junit.Test; 
-import java.io.StringReader;
+import java.io.*;
 
 public class RJsonLexerMain {
     static Logger logger = LogManager.getLogger(RJsonLexerMain.class);
@@ -30,14 +27,14 @@ public class RJsonLexerMain {
             // System.out.println(logger.isDebugEnabled());
             // logger.error("starting...");
             // System.out.println("Starting...");
-            JSONParser parser = new JSONParser();
-            JSONObject a = (JSONObject) parser.parse(new FileReader("./test.json"));
+            // 
             //System.out.println(a.toString());
             
             String json = "{\"name\":\"sonoo\",\"salary\":600000.0,\"age\":27}";
             //String json = "{\"name\":\"sonoo\",\"age\":0.1}";
             // String json = " {\"name\"   :\"son oo\"   ,\"male\":null,\"age\":{\"age\":22,\"gh\":\"abcd\"} }";
-            RJsonParser instance = new RJsonParser(a.toString());
+            //RJsonParser instance = new RJsonParser(a.toString());
+            RJsonParser instance = new RJsonParser(new FileInputStream("./test.json"));
             StringBuilder builder = new StringBuilder();
             instance.parse().toHtml(builder,-1);
             System.out.println(builder.toString());
