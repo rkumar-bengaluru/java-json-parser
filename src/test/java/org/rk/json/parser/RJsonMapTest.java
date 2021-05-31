2e7258aa493647630f687892bbb9d9d659343af9
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-05-31 18:24:32 +0530

    map test

--- src/test/java/org/rk/json/parser/RJsonMapTest.java
+++ src/test/java/org/rk/json/parser/RJsonMapTest.java
@@ -2,6 +2,7 @@ package org.rk.json.parser;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.rk.json.pojo.JsonMap;
 import org.rk.json.pojo.JsonList;
 import org.rk.json.pojo.JsonString;
 import org.rk.json.pojo.JsonObjType;
@@ -10,20 +11,20 @@ import org.junit.Test;
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertEquals;
 
-public class RJsonListTest {
+public class RJsonMapTest {
     static Logger logger = LogManager.getLogger(RJsonListTest.class);
 
     @Test  
     public void testList01(){  
         try {
-            JsonList instance = new JsonList(JsonObjType.LIST);
+            JsonMap instance = new JsonMap(JsonObjType.MAP);
             instance.setRoot();
-            instance.add(new JsonString(JsonObjType.STRING,"fivesstar"));
-            instance.add(new JsonString(JsonObjType.STRING,"ratings"));
-            StringBuilder appendable = new StringBuilder();
-            instance.toString(appendable,-1);
-            System.out.println("response = " + appendable.toString());
-            assertEquals("\n[\n\"fivesstar\",\"ratings\"\n]",appendable.toString());
+            // instance.add(new JsonString(JsonObjType.STRING,"fivesstar"));
+            // instance.add(new JsonString(JsonObjType.STRING,"ratings"));
+            // StringBuilder appendable = new StringBuilder();
+            // instance.toString(appendable,-1);
+            // System.out.println("response = " + appendable.toString());
+            // assertEquals("\n[\n\"fivesstar\",\"ratings\"\n]",appendable.toString());
         } catch(Exception e) {
             fail(e.getMessage());
         }
